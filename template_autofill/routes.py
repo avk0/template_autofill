225e5fb29d06af308e35e3c18dfead6d0ec94a5e
Timerxan <tfalyakhov@gmail.com>
2022-05-02 19:22:36 +0300

    DEFAULT_LANG get_dir_size index

--- template_autofill/routes.py
+++ template_autofill/routes.py
@@ -37,7 +37,7 @@ def get_dir_size(path):
         if not os.path.isdir(os.path.join(path, x)):
             size += os.stat(os.path.join(path, x)).st_size
         else:
-            size += recursive_dir_size(os.path.join(path, x))
+            size += get_dir_size(os.path.join(path, x))
     return size
 
 
